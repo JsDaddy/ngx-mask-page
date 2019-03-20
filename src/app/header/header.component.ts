86f90526b3bc6f2a7130610dabd8c06dd075d143
ValeriaKochegarova <lerchik199629@gmail.com>
2019-03-20 18:16:27 +0200

    created main page

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,23 +1,9 @@
-<<<<<<< HEAD
 import { Component } from '@angular/core';
-=======
-import { Component, OnInit } from '@angular/core';
->>>>>>> b66bf42d93d1919c216cc1abf3259d8b9bd375d0
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-<<<<<<< HEAD
 export class HeaderComponent  {
-=======
-export class HeaderComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
->>>>>>> b66bf42d93d1919c216cc1abf3259d8b9bd375d0
 }
--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,17 +1,11 @@
 import { NgModule } from '@angular/core';
-<<<<<<< HEAD
 import { MatIconModule } from '@angular/material/icon';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
-=======
-import {MatIconModule} from '@angular/material/icon';
-import {MatToolbarModule} from '@angular/material/toolbar';
->>>>>>> b66bf42d93d1919c216cc1abf3259d8b9bd375d0
 
 @NgModule({
   imports: [
     MatIconModule,
-<<<<<<< HEAD
     MatToolbarModule,
     MatButtonModule
   ],
@@ -19,13 +13,6 @@ import {MatToolbarModule} from '@angular/material/toolbar';
     MatIconModule,
     MatToolbarModule,
     MatButtonModule
-=======
-    MatToolbarModule
-  ],
-  exports: [
-    MatIconModule,
-    MatToolbarModule
->>>>>>> b66bf42d93d1919c216cc1abf3259d8b9bd375d0
   ]
 })
 export class MaterialModule { }
