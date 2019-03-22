d3ab99b93019998fd0c418ecce9c7be919b0bbdf
thegrep01 <den.belotserkovets@gmail.com>
2019-03-22 14:50:43 +0200

    refactor

---
+++ src/assets/content/lists.ts
@@ -0,0 +1,33 @@
+// tslint:disable-next-line: no-any
+export const lists: any[] = [
+    {
+        header: 'Options',
+        id: '1',
+        text: [
+            {
+                content: 'Prefix',
+                id: '1'
+            },
+            {
+                content: 'Suffix',
+                id: '2'
+            },
+            {
+                content: 'dropSpecialCharacters',
+                id: '3'
+            },
+            {
+                content: 'showMaskTyped',
+                id: '4'
+            },
+            {
+                content: 'clearIfNotMatch',
+                id: '5'
+            },
+            {
+                content: 'Validation',
+                id: '6'
+            },
+        ],
+    },
+];
