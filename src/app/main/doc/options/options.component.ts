e6f9a46e2170b123e329d2796190c6509e9fa49a
Ihor <igornepipenko@gmail.com>
2020-10-19 12:45:00 +0300

    Feature: update deps angular v10 [ref:no-ref]

--- src/app/main/doc/options/options.component.ts
+++ src/app/main/doc/options/options.component.ts
@@ -1,5 +1,4 @@
 import { Component, Input } from '@angular/core';
-import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-options',
@@ -8,15 +7,20 @@ import { FormControl } from '@angular/forms';
 })
 export class OptionsComponent {
   // tslint:disable
-  @Input('docs') public docs: any[];
-  @Input('examples') public examples: any[];
-  @Input('choose') public choose: string;
+  @Input('docs')
+  public docs!: any[];
+
+  @Input('examples')
+  public examples!: any[];
+
+  @Input('choose')
+  public choose!: string;
+
   public phone: string = '123456789'
   public customPatterns = { '0': { pattern: new RegExp('\[a-zA-Z\]') } };
 
   public checkChoose(input: string, curr: string): boolean {
-
-    return input === curr ? true : false;
+    return input === curr;
   }
 
 
